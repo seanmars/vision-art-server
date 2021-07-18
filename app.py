c503caf8891a45c742d6cd83b8f02de6d4c8d5ea
seanmars <mars0222@gmail.com>
2021-07-19 00:03:13 +0800

    修正沒資料的時候造成建立 PaletteSearchIndex 失敗的問題

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ from palette_embedding import PaletteEmbeddingModel, PaletteSearchIndex
 app = Flask(__name__)
 data_manager = DataManager()
 model = PaletteEmbeddingModel()
-palette_index = PaletteSearchIndex(model, data_manager.get_data())
+data = data_manager.get_data()
+palette_index = PaletteSearchIndex(model, data) if len(data) > 0 else None
 default_num_neighbors = 10
 
 
@@ -20,12 +21,18 @@ def hello():
 @app.route('/palette/<palette>', methods=['GET', 'POST'])
 def request_similar_vision(palette: str):
     num_neighbors = request.args.get('num', default=default_num_neighbors, type=int)
-    indices, distances = palette_index.GetNearestNeighbors(palette, num_neighbors)
-    image_infos = data_manager.get_palettes(indices)
-    return {
-        'images': image_infos,
-        'distances': distances
-    }
+    if palette_index is None:
+        return {
+            'images': [],
+            'distances': []
+        }
+    else:
+        indices, distances = palette_index.GetNearestNeighbors(palette, num_neighbors)
+        image_infos = data_manager.get_palettes(indices)
+        return {
+            'images': image_infos,
+            'distances': distances
+        }
 
 
 if __name__ == '__main__':
